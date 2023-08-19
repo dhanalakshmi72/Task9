f24db29dd377b8845b1b3d9b7737b7e947fb063c
iniya77 <iniya772002@gmail.com>
2023-08-19 18:50:51 +0530

    seccond

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2023 Your Meditation App. All rights reserved.</p>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis aperiam at nobis, nulla impedit incidunt, quasi cupiditate, ea voluptatem voluptates consectetur. Saepe veritatis laborum illum reprehenderit suscipit sit animi neque magni explicabo officia natus non, earum exercitationem tempore sed deleniti. Sapiente sequi incidunt maxime nobis at. Tempore quo veritatis consequatur a porro architecto pariatur consectetur assumenda quaerat blanditiis enim ut nobis sed modi, officiis est dolorum aspernatur saepe culpa aliquid quasi delectus labore dignissimos in? Ducimus sed ipsa necessitatibus blanditiis inventore veritatis exercitationem illum rem omnis rerum ea laudantium repellendus, perspiciatis magni tempore eveniet aperiam soluta. Porro illo facere possimus beatae asperiores, maiores similique nihil nostrum nobis rem ratione vel? Assumenda a ducimus nemo delectus repudiandae quaerat provident, nulla alias.</p>
     </footer>
   );
 }
